8331472f14442ee858e062e44b799d627852b15e
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-10-21 21:00:03 +0900

    Userモデル単体テストコードexample

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザーの新規登録' do
+    context 'ユーザーの新規登録フォームを正常に行えばば新規登録できる' do
+      
+    end
+
+    context 'ユーザーの新規登録フォームが異常であれば新規登録できない' do
+      it 'ニックネームが空欄' do
+      end
+      
+      it 'メールアドレスが空欄' do
+      end
+
+      it 'メールアドレスが既に登録されているものである' do
+      end
+
+      it 'メールアドレスに@がない' do
+      end
+
+      it 'パスワードが空欄' do 
+      end
+
+      it 'パスワードが6文字未満' do
+      end
+
+      it 'パスワードが数字のみ' do
+      end
+
+      it 'パスワードの確認が空欄' do
+      end
+
+      it 'パスワードの確認の値がパスワードと不一致' do
+      end
+
+      it '名字が空欄' do
+      end
+
+      it '名前が空欄' do
+      end
+
+      it '名字(カナ)が空欄' do
+      end
+
+      it '名前(カナ)が空欄' do
+      end
+
+      it 'ユーザー本名が半角で記入' do
+      end
+
+      it 'ユーザー本名(カナ)が全角で記入' do
+      end
+
+      it '生年月日が空欄' do
+      end
+    end
+  end
 end
