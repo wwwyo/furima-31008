b7363734f2db71f7a0c1c982b3c9babbf92fb533
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-10-22 13:23:37 +0900

    商品出品機能コントローラーアクション記入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.create
+  end
+
+  private
+  def item_params
+  end
+
 end
