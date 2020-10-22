8c5869305b732300d5b98b64ca5e74132dc4d2aa
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-10-22 15:41:51 +0900

    ItemモデルにActiveHashのアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  belongs_to_active_hash :genre
+  belongs_to_active_hash :state
+  belongs_to_active_hash :shipping_fee
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :day
 end
