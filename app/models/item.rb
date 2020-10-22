b38c8a009050310f2de79a11f27f0dfb5b2ecacd
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-10-22 15:55:00 +0900

    active storageアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  has_one_attached :image
   belongs_to_active_hash :genre
   belongs_to_active_hash :state
   belongs_to_active_hash :shipping_fee
