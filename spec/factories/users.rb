98c5066279a116e0812b3e4872b8fe2904a70960
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-10-22 09:48:05 +0900

    userモデルfactorybot作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :user do
-    
+    transient do
+      person { Gimei.name }
+    end
+    email { Faker::Internet.email }
+    password = Faker::Internet.password(min_length: 6, mix_case: true)
+    password { password }
+    password_confirmation { password }
+    nickname { Faker::Name.name }
+    last_name { person.last.kanji }
+    first_name { person.first.kanji }
+    last_name_kana { person.last.katakana }
+    first_name_kana { person.first.katakana }
+    birth_day { Faker::Date.between(from: '1930-01-01', to: '2015-12-31') }
   end
 end
