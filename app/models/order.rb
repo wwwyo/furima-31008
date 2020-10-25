56a407c9e72cc5ddb63fb17737d15bede97bb58b
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-10-25 12:18:50 +0900

    active_hashアソシエーション

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   belongs_to :item
   has_one :address
+  belongs_to_active_hash :prefecture
 end
