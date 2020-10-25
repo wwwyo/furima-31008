a36a01e8d995cd3aa437b431b06529039c773a99
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-10-25 18:23:28 +0900

    トークンの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,6 +7,7 @@ class OrdersController < ApplicationController
   def create
     @order_form = OrderForm.new(order_form_params)
     if @order_form.valid?
+      pay_item
       @order_form.save
       redirect_to root_path
     else
@@ -15,10 +16,19 @@ class OrdersController < ApplicationController
       render :index
     end
   end
-  
+
   private
   
   def order_form_params
-    params.require(:order_form).permit(:postal_code, :delivery_prefecture_id, :delivery_town, :delivery_number, :delivery_building, :phon_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_form).permit(:postal_code, :delivery_prefecture_id, :delivery_town, :delivery_number, :delivery_building, :phon_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: Item.find(params[:item_id]).price,
+      card: order_form_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,7 +1,7 @@
 class OrderForm
 
   include ActiveModel::Model
-  attr_accessor :postal_code, :delivery_prefecture_id, :delivery_town, :delivery_number, :delivery_building, :phon_number, :user_id, :item_id
+  attr_accessor :postal_code, :delivery_prefecture_id, :delivery_town, :delivery_number, :delivery_building, :phon_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
@@ -9,6 +9,7 @@ class OrderForm
     validates :delivery_town
     validates :delivery_number
     validates :phon_number, format: { with: /\A[0-9]{11}\z/ }
+    validates :token
   end
 
   def save
