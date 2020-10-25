c12214b53cefe98eca48184720c802a8c48e1694
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-10-25 10:08:38 +0900

    addressesテーブル作成

--- db/migrate/20201025005404_create_addresses.rb
+++ db/migrate/20201025005404_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.string :postal_code, null: false
+      t.integer :delivery_prefecture_id, null: false
+      t.string :delvery_town, null: false
+      t.string :delivery_number, null: false
+      t.string :delivery_building
+      t.string :phon_number, null: false
+      t.references :order, null: false, foreign_key: true
       t.timestamps
     end
   end
