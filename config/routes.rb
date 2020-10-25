96110a6f64a2f0c1dfe474f061c9f2795beb2b5a
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-10-25 10:44:06 +0900

    ordersルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'orders/index'
   root to: 'items#index'
   devise_for :users
-  resources :items
+  resources :items do
+    resources :orders, only: :index
+  end 
 end
